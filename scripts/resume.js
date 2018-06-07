f16ff7ebcb689dafcb9e42729f19d4bcbf256749
Laginalin <laginalin@gmail.com>
2018-06-07 19:06:31 +0800

    add resume js

---
+++ scripts/resume.js
@@ -0,0 +1,18 @@
+function changeStyle1(){
+  var change = document.getElementById("styleInfo");
+  change.setAttribute("href","styles/resume_style_1.css");
+}
+
+function changeStyle2(){
+  var change = document.getElementById("styleInfo");
+  change.setAttribute("href","styles/resume_style_2.css");
+}
+
+function changeStyle3(){
+  var change = document.getElementById("styleInfo");
+  change.setAttribute("href","styles/resume_style_3.css");
+}
+
+function specialTime(){
+  alert("动态修改CSS样式方法汇总：https://www.cnblogs.com/anstoner/p/8012730.html");
+}
